4cffa2ec769b1611d7c95043727f854b0ee29f30
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-10 14:47:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,3 +4,16 @@
 def key_for_min_value(name_hash)
   
 end
+
+
+def key_for_min_value(name_hash)
+  min_key = nil
+  min_value = 1000
+  name_hash.each do |key, value|
+    if value < min_value
+      min_key = key
+      min_value = value
+    end #if
+  end #do
+  min_key
+end #key_for_min_value
